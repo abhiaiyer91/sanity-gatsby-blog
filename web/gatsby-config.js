63c95d9ff6cd43dcea09f58129c78e0c4d755f46
abhiaiyer91 <abhiaiyer91@gmail.com>
2019-09-30 11:48:36 +0100

    update stuff

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -7,7 +7,7 @@ const clientConfig = require('./client-config')
 
 const isProd = process.env.NODE_ENV === 'production'
 
-const foo = `bar!!!`
+const foo = `bar!!!!!`
 
 module.exports = {
   plugins: [
